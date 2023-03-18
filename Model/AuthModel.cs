44af8ac1a6ffa49d3a7d3d27914875526a13a624
blackaly <algeserone@yandex.com>
2023-03-18 17:45:44 +0200

    register

--- Model/AuthModel.cs
+++ Model/AuthModel.cs
@@ -6,6 +6,7 @@ namespace MovieAPI.Model
         public string Message { get; set; }
         public bool IsAuthenticated { get; set; }
         public string Username { get; set; }
+        public List<string> Roles { get; set; }
         public string Email { get; set; }
         public string Token { get; set; }   
         public DateTime Expiration { get; set; }
--- Services/Implementation/AuthService.cs
+++ Services/Implementation/AuthService.cs
@@ -1,13 +1,104 @@
-﻿using MovieAPI.Model;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.CodeAnalysis.Options;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using MovieAPI.JWT;
+using MovieAPI.Model;
+using MovieAPI.Model.Domains;
+using MovieAPI.Model.Enums;
 using MovieAPI.Services.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace MovieAPI.Services.Implementation
 {
     public class AuthService : IAuthService
     {
-        public Task<AuthModel> Register(RegisterationModel model)
-        {
 
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly JwtMapping _jwt; 
+
+        public AuthService(UserManager<ApplicationUser> userManager, IOptions<JwtMapping> jwt) {
+            _userManager = userManager;
+            _jwt = jwt.Value;
+        }
+
+        public async Task<AuthModel> Register(RegisterationModel model)
+        {
+            if (await _userManager.FindByEmailAsync(model.Email) == null)
+                return new AuthModel() { Message = "Email is already exists" };
+
+            if (await _userManager.FindByNameAsync(model.Username) == null)
+                return new AuthModel() { Message = "Username is already exists" };
+
+
+            var usr = new ApplicationUser()
+            {
+                UserName = model.Username,
+                Email = model.Email,
+                Name = model.FirtName + " " + model.LastName
+            };
+
+            var res = await _userManager.CreateAsync(usr, model.Password);
+
+            if (!res.Succeeded)
+            {
+                string errors = string.Empty;
+
+                foreach(var err in res.Errors) errors += err + "\n";
+
+                return new AuthModel() { Message = errors };
+            }
+
+            await _userManager.AddToRoleAsync(usr, nameof(Roles.User));
+
+            var token = await CreateJwtToken(usr);
+
+            var auth = new AuthModel()
+            {
+                Email = model.Email,
+                Username = model.Username,
+                Expiration = token.ValidTo,
+                IsAuthenticated = true,
+                Message = string.Empty,
+                Roles = new List<string>() { "User" },
+                Token = new JwtSecurityTokenHandler().WriteToken(token)
+            };
+
+            return auth;
+        }
+
+        private async Task<JwtSecurityToken> CreateJwtToken(ApplicationUser user)
+        {
+            var userClaims = await _userManager.GetClaimsAsync(user);
+            var roles = await _userManager.GetRolesAsync(user);
+            var roleClaims = new List<Claim>();
+
+            foreach (var role in roles)
+                roleClaims.Add(new Claim("roles", role));
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim("uid", user.Id)
+            }
+            .Union(userClaims)
+            .Union(roleClaims);
+
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwt.Key));
+            var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+
+            var jwtSecurityToken = new JwtSecurityToken(
+                issuer: _jwt.Issuer,
+                audience: _jwt.Audience,
+                claims: claims,
+                expires: DateTime.Now.AddDays(_jwt.DurationInDays),
+                signingCredentials: signingCredentials);
+
+            return jwtSecurityToken;
         }
     }
 }
