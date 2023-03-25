1ff542801296c53122a8a8324ac5d083754c4ed4
blackaly <algeserone@yandex.com>
2023-03-25 22:17:18 +0200

    .

--- Model/ApplicationDbContext.cs
+++ Model/ApplicationDbContext.cs
@@ -9,7 +9,14 @@ namespace MovieAPI.Model
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Director> Directors { get; set; }
         public DbSet<Movie> Movies { get; set; }
-
+        public DbSet<Rating> Ratings { get; set; }
+        public DbSet<Recommendation> Recommendations { get; set; }
+        public DbSet<Review> Reviews{ get; set; }
+        public DbSet<Series> Series { get; set; }
+        public DbSet<Watchlist> Watchlists { get; set; }
+        public DbSet<Subscription> Subscriptions { get; set; }
     }
 }
